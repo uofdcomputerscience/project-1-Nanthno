ad06d9bfb692a99b41a2088c94613aa790059488
Anthony Hanson <ahanson@udallas.edu>
2019-10-03 16:19:56 -0700

    create image table and add code to read from url

---
+++ MercuryBrowser/ImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCell.swift
+//  MercuryBrowser
+//
+//  Created by Anthony Hanson on 10/3/19.
+//  Copyright © 2019 Russell Mirabelli. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+}
